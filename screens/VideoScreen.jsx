917a1cf6ec86890b9d464dd663d89f67c95fd160
LAPTOP-1UI46O0B\hi090 <hi09067@gmail.com>
2025-08-07 02:34:45 +0900

    영상 코드 수정

--- screens/VideoScreen.jsx
+++ screens/VideoScreen.jsx
@@ -1,23 +1,35 @@
-import { useNavigation } from '@react-navigation/native';
-import React from 'react';
-import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { WebView } from 'react-native-webview';
-
-export default function VideoScreen() {
-  const navigation = useNavigation();
-
+export default function VideoScreen({ navigation }) {
   const handleNext = () => {
     navigation.navigate('SeatCheck');
   };
 
+  if (Platform.OS === 'web') {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <iframe
+          width="100%"
+          height={Dimensions.get('window').height * 0.6}
+          src="https://www.youtube.com/embed/Ypo9PkdTBCY"
+          title="YouTube video"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+
+        <TouchableOpacity style={styles.button} onPress={handleNext}>
+          <Text style={styles.buttonText}>넘어가기</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
       <WebView
-        style={{ flex: 1 }}
         javaScriptEnabled={true}
         source={{ uri: 'https://www.youtube.com/embed/Ypo9PkdTBCY' }}
-        />
-
+        style={{ flex: 1 }}
+      />
       <TouchableOpacity style={styles.button} onPress={handleNext}>
         <Text style={styles.buttonText}>넘어가기</Text>
       </TouchableOpacity>
@@ -25,15 +37,12 @@ export default function VideoScreen() {
   );
 }
 
-const { height } = Dimensions.get('window');
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-  },
-  video: {
-    height: height * 0.6,
   },
   button: {
     position: 'absolute',
