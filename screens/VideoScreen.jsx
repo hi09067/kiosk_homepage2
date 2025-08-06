7f65597d428ffd3496f0c4ca9a5e59ca347175a8
LAPTOP-1UI46O0B\hi090 <hi09067@gmail.com>
2025-08-07 02:27:40 +0900

    stylesheet 추가

--- screens/VideoScreen.jsx
+++ screens/VideoScreen.jsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function VideoScreen() {
